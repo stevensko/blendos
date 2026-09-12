88f30db73c0ae1d2f7d5fd24981d0b5d9148c5a5
stevensko <30204691+stevensko@users.noreply.github.com>
2026-09-11 21:58:51 -0400

    update dotfiles

--- .config/akshara/post.sh
+++ .config/akshara/post.sh
@@ -7,6 +7,8 @@ magick -size 1x1 xc:transparent /usr/share/plymouth/themes/spinner/watermark.png
 
 # GDM
 mkdir -p /etc/gdm-shell
+magick -size 1x1 xc:transparent /usr/share/pixmaps/archlinux-logo-text-dark.svg
+magick -size 1x1 xc:transparent /usr/share/icons/cachyos.svg
 cp /usr/share/gnome-shell/gnome-shell-theme.gresource /etc/gdm-shell/gnome-shell-theme.gresource
 chmod u=rw,go=r /etc/gdm-shell/gnome-shell-theme.gresource
 cp /usr/share/gnome-shell/gnome-shell-theme.gresource /etc/gdm-shell/gnome-shell-theme.gresource.default
